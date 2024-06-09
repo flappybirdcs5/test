f154c2443cc4f218b19d8b7620d81827cab26e28
Piyush <chandra.piyush17@gmail.com>
2024-06-09 14:09:03 +0530

    test

---
+++ calc.js
@@ -0,0 +1,17 @@
+function add(x,y) {
+	console.log('add')
+}
+
+function substract(x,y) {
+	console.log("substract")
+}
+
+
+function divide(x,y) {
+	console.log("divide")
+}
+
+
+function multiply(x,y) {
+	console.log("multiply")
+}
